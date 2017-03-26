dbbd95126c451747be7354f55309319e0ebfd9f8
Alexandre Di Guida <alexandre.diguida@gmail.com>
2017-03-26 13:17:13 +0100

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -14,9 +14,42 @@ module.exports = {
     try {
       todos = JSON.parse(stringTodos);
     } catch (e) {
-      log.e(e.toString());
+
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    // Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var todoText = todo.text.toLowerCase();
+      return searchText.lenght === 0 || todoText.indexOf(searchText) > -1; // > -1 is success
+    });
+
+    // Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+
+      // RULES
+      // -1 : a should come before b
+      // 1 : b should come before a
+      // 0 : no change
+
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    return filteredTodos;
   }
 };
